a8a48795991d144780ca8901485bc5324b4be2ae
Omar <omar.altardi@maqsam.com>
2026-06-07 14:51:45 +0300

    added session controller, login filtering and session handling

--- shelf/app/controllers/application_controller.rb
+++ shelf/app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+  helper_method :current_user, :logged_in?
+  before_action :require_login 
+  def require_login
+    unless logged_in?
+      redirect_to "/login", alert: "You must be logged in to access this page."
+    end
+  end
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+  def logged_in?
+    !!current_user
+  end
 end
