a07261cb464e1655d4177f4af44777d573067ca4
Omar <omar.altardi@maqsam.com>
2026-06-07 11:45:49 +0300

    Added model level validations for book, user and review models

--- shelf/app/models/book.rb
+++ shelf/app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   has_many :reviews, dependent: :destroy
+  validates :title, presence: true
 end
--- shelf/app/models/user.rb
+++ shelf/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :reviews, dependent: :destroy
+  has_many :reviewed_books, through: :reviews, source: :book
+  validates :email, presence: true, uniqueness: true
 end
